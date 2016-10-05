4c8242fa7efe81d990aa6e82a9da0fc89b21d1b6
KodyLeonard <kody.leonard@okstate.edu>
2016-10-05 12:29:26 -0500

    created fucntion for gc content

--- ASS 8 houseelf-analysis.R
+++ ASS 8 houseelf-analysis.R
@@ -1,3 +1,8 @@
 #Analysis houseelf data
+library(stringr)
 houseelf <- read.csv("houseelf_earlength_dna_data_1.csv")
-str_to_upper(houseelf$dnaseq)
+gc_c <- function(sequence){
+  Gs <- str_count(sequence, "G")
+  Cs <- str_count(sequence, "C")
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100
+  return(gc_content)
