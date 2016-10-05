ea83ab56e52465f865061402298ac6762faf2ae1
KodyLeonard <kody.leonard@okstate.edu>
2016-10-05 12:53:02 -0500

    Calculated GC content for houseelf

--- ASS_8_houseelf-analysis.R
+++ ASS_8_houseelf-analysis.R
@@ -1,8 +1,10 @@
 #Analysis houseelf data
 library(stringr)
 houseelf <- read.csv("houseelf_earlength_dna_data_1.csv")
-gc_c <- function(sequence){
-  Gs <- str_count(sequence, "G")
-  Cs <- str_count(sequence, "C")
-  gc_content <- (Gs + Cs) / str_length(sequence) * 100
-  return(gc_content)
+gc_content <- function(sequence){
+  Gs <- str_count(sequence, "g")
+  Cs <- str_count(sequence, "c")
+  gc <- round((Gs + Cs) / str_length(sequence) * 100, digits = 2) 
+  return(gc)
+}
+print(gc_content(str_to_lower(houseelf$dnaseq)))
